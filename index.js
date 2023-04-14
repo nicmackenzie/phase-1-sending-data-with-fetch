932f65751dac4480acb91b58c86510e7b30ea106
nicmackenzie <nicmackenzie@live.com>
2023-04-14 15:52:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch(`http://localhost:3000/users`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+    body: JSON.stringify({ name, email }),
+  })
+    .then(res => res.json())
+    .then(data => {
+      const h1 = document.createElement('h1');
+      h1.textContent = data.id;
+      document.body.appendChild(h1);
+    })
+    .catch(err => {
+      const div = document.createElement('div');
+      div.textContent = err.message;
+      document.body.appendChild(div);
+    });
+}
